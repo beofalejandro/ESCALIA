1b291d781b2374d2052aeb8eb8f58774ee912c76
AlejandroLMR <alexalejandro4141@gmail.com>
2023-12-16 09:18:15 -0600

    Peformance and Style Update

--- scripts/institute-script.js
+++ scripts/institute-script.js
@@ -1,9 +1,12 @@
 let currentIndex = 0;
+
+// Theme variables
 let ButtonMode = document.getElementById('mode');
-let themeforbody = document.body;
-let themeforfooter = document.getElementById('footer');
+themeforbody = document.body;
+themeforfooter = document.getElementById('footer');
+themeforcontainers = document.getElementById('institute-container') // Preview
 
-
+// Automatic slider
 function showSlide(index) {
     const slider = document.querySelector('.slider');
     const slides = document.querySelectorAll('.slide');
@@ -12,12 +15,14 @@ function showSlide(index) {
     slider.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
+// Button next slide
 function nextSlide() {
     const totalSlides = document.querySelectorAll('.slide').length;
     currentIndex = (currentIndex + 1) % totalSlides;
     showSlide(currentIndex);
 }
 
+// Button prev slide
 function prevSlide() {
     const totalSlides = document.querySelectorAll('.slide').length;
     currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
@@ -26,7 +31,10 @@ function prevSlide() {
 
 setInterval(nextSlide, 15000);
 
+
+// Change theme
 ButtonMode.addEventListener('click', () => {
         themeforbody.classList.toggle('light');
         themeforfooter.classList.toggle('light');
+        themeforcontainers.classList.toggle('light'); // Preview
     });
