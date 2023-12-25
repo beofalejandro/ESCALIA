6d38e0fa178144774e7a7f0516a70299a06a6c57
AlejandroLMR <alexalejandro4141@gmail.com>
2023-12-25 17:29:22 -0600

    Fix content
    Fix some links buttons and add little content, delete some bad code and change the backgrounds

--- scripts/laboratory.js
+++ scripts/laboratory.js
@@ -8,29 +8,3 @@ window.addEventListener('scroll', () => {
         header.style.backgroundColor = 'transparent'
     }
 })
-
-///// SLIDER
-let currentIndex = 0;
-
-function showSlide(index) {
-    const slider = document.querySelector('.slider');
-    const slides = document.querySelectorAll('.slide');
-    currentIndex = index;
-
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-function nextSlide() {
-    const totalSlides = document.querySelectorAll('.slide').length;
-    currentIndex = (currentIndex + 1) % totalSlides;
-    showSlide(currentIndex);
-}
-
-function prevSlide() {
-    const totalSlides = document.querySelectorAll('.slide').length;
-    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    showSlide(currentIndex);
-}
-
-///// Automático
-setInterval(nextSlide, 10000);
