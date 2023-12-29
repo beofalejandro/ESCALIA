3b0b982e847c429d17d5c497104dbcecc9fb1606
AlejandroLMR <alexalejandro4141@gmail.com>
2023-12-29 17:35:56 -0600

    Little beta Update
    I wannna use panels in my webapp because i go to learn when use it

--- scripts/laboratory.js
+++ scripts/laboratory.js
@@ -8,3 +8,26 @@ window.addEventListener('scroll', () => {
         header.style.backgroundColor = 'transparent'
     }
 })
+
+// PANELS 
+const contenedor = document.getElementById("contenedor");
+const panel1 = document.getElementById("panel1");
+const panel2 = document.getElementById("panel2");
+const boton1 = document.getElementById("boton1");
+const boton2 = document.getElementById("boton2");
+
+// Inicialmente, solo se muestra el panel 1
+panel1.style.display = "block";
+panel2.style.display = "none";
+
+boton1.addEventListener("click", () => {
+    // Muestra el panel 1
+    panel1.style.display = "block";
+    panel2.style.display = "none";
+});
+
+boton2.addEventListener("click", () => {
+    // Muestra el panel 2
+    panel1.style.display = "none";
+    panel2.style.display = "block";
+});
