d0604280d2aa4a96e59c0bf8b449184cda4c4f73
AlejandroLMR <112975406+AlejandroLMR@users.noreply.github.com>
2024-01-16 16:38:17 -0600

    Fix content and Change the footer
    Lets to change more from my webapp for example colors palette and footer

--- scripts/institute-script.js
+++ scripts/institute-script.js
@@ -1,7 +1,6 @@
 let currentIndex = 0;
-
 // Theme variables
-let ButtonMode = document.getElementById('mode');
+let ButtonMode = document.getElementById('theme-mode');
 themeforbody = document.body;
 themeforfooter = document.getElementById('footer');
 themeforcontainers = document.getElementById('institute-container') // Preview
@@ -11,7 +10,6 @@ function showSlide(index) {
     const slider = document.querySelector('.slider');
     const slides = document.querySelectorAll('.slide');
     currentIndex = index;
-
     slider.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
@@ -28,13 +26,11 @@ function prevSlide() {
     currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
     showSlide(currentIndex);
 }
-
 setInterval(nextSlide, 15000);
-
 
 // Change theme
 ButtonMode.addEventListener('click', () => {
-        themeforbody.classList.toggle('light');
-        themeforfooter.classList.toggle('light');
-        themeforcontainers.classList.toggle('institute-container-light'); // Preview
-    });
+    themeforbody.classList.toggle('body-light');
+    themeforfooter.classList.toggle('footer-light');
+    themeforcontainers.classList.toggle('institute-container-light'); // Preview
+});
--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var header = document.getElementById('header');
 window.addEventListener('scroll', () => {
     var scroll = window.scrollY
     if (scroll > 10) {
-        header.style.backgroundColor = '#252525'
+        header.style.backgroundColor = '#212121'
     } else {
         header.style.backgroundColor = 'transparent'
     }
