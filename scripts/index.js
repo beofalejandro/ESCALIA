1d3d6901cede68ff68cdbe9120cad9f31021bfad
AlejandroLMR <112975406+AlejandroLMR@users.noreply.github.com>
2024-01-19 13:21:32 -0600

    Animations and Colors update part II
    I add some animations in index page and change the colors, the last is a part to colors update

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal();
+
+// ANIMATIONS WITH USER SCROLL
+sr.reveal('.w_content', {
+    duration: 1200,
+    origin: 'bottom',
+    reset: 'true'
+});
+
+sr.reveal('.cards-container', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-30px',
+});
+
+sr.reveal('.longest-cards-space', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-30px',
+});
