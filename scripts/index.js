1c4a8a5669d63ddfe876bd0e95e0e4a9be2a690d
Alejandro Barrientos <112975406+beofalejandro@users.noreply.github.com>
2024-05-10 09:21:16 -0600

    Performance Update 3.4 Pre-release
    A lot of changes added in the final version mentioned it

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,42 @@
 window.sr = ScrollReveal();
 
+// PANELS WITH JS
+const contenedor = document.getElementById("repositories-container"); //GET THE CONATINER FOR PANELS
+// SET THE DIV's
+const panel1 = document.getElementById("repo_pan_1");
+const panel2 = document.getElementById("repo_pan_2");
+const panel3 = document.getElementById("repo_pan_3");
+// SET THE ACTION BUTTONS
+const boton1 = document.getElementById("button_repo_1");
+const boton2 = document.getElementById("button_repo_2");
+const boton3 = document.getElementById("button_repo_3");
+
+// SET PANEL1 FOR DEFAULT
+panel1.style.display = "block";
+panel2.style.display = "none"; //HIDE PANEL2
+panel3.style.display = "none";
+
+// IF YOU GO BACK TO PANEL1
+boton1.addEventListener("click", () => {
+    panel1.style.display = "block";
+    panel2.style.display = "none";
+    panel3.style.display = "none";
+});
+
+// IF YOU CLICK THE BUTTON SHOW PANEL2
+boton2.addEventListener("click", () => {
+    panel1.style.display = "none";
+    panel2.style.display = "block";
+    panel3.style.display = "none";
+});
+
+// IF YOU CLICK THE BUTTON SHOW PANEL3
+boton3.addEventListener("click", () => {
+    panel1.style.display = "none";
+    panel2.style.display = "none";
+    panel3.style.display = "block";
+});
+
 // ANIMATIONS WITH USER SCROLL
 sr.reveal('.index_title', {
     duration: 1500,
@@ -20,6 +57,13 @@ sr.reveal('.longest-cards-container', {
 });
 
 sr.reveal('.color-palette', {
+    duration: 2000,
+    delay: 200,
+    origin: 'bottom',
+    distance: '-20px',
+});
+
+sr.reveal('.escalia-products', {
     duration: 2000,
     delay: 200,
     origin: 'bottom',
--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var header = document.getElementById('header');
 window.addEventListener('scroll', () => {
     var scroll = window.scrollY
     if (scroll > 15) {
-        header.style.backgroundColor = '#212121'
+        header.style.backgroundColor = '#151515'
     } else {
         header.style.backgroundColor = 'transparent'
     }
